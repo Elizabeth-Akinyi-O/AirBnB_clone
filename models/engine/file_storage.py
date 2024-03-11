61f9b2a3a7ee5ef7e4eab7c8effece61d492d7f4
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2024-03-11 07:06:50 +0300

    FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """ Engine that is incharge of serializing/deserializing objects to files """
 import json
 import os
@@ -11,46 +12,32 @@ class FileStorage():
 
     def all(self):
         """ Returns dictionaries """
-        return (FileStorage.__objects)
+        return (self.__objects)
 
     def new(self, obj):
         """ Creates a new object """
-        className = type(obj).__name__
-        myId = obj.id
-        instanceKey = className + "." + myId
-        FileStorage.__objects[instanceKey] = obj
+
+        Key = str(obj.__class__.__name__) + "." + str(obj.id)
+        value_dict = obj
+        FileStorage.__objects[Key] = value_dict
 
     def save(self):
         """ Saves to a file in json format """
         myObjDict = {}
-        for key in FileStorage.__objects:
-            myObjDict[key] = FileStorage.__objects[key].to_dict()
-        with open(FileStorage.__file_path, 'w') as file_path:
-            json.dump(myObjDict, file_path)
+        for key, val in FileStorage.__objects.items():
+            myObjDict[key] = val.to_dict()
+
+        with open(FileStorage.__file_path, mode='w', encoding="UTF8") as fd:
+            json.dump(myObjDict, fd)
 
     def reload(self):
         """ Loads from json file """
-        from models.base_model import BaseModel
-        from models.amenity import Amenity
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.place import Place
-        from models.review import Review
-        myDict = {
-            "BaseModel": BaseModel,
-            "Amenity": Amenity,
-            "User": User,
-            "State": State,
-            "City": City,
-            "Place": Place,
-            "Review": Review
-            }
-        if not os.path.isfile(FileStorage.__file_path):
-            return
-        with open(FileStorage.__file_path, 'r') as file_path:
-            objects = json.load(file_path)
-            FileStorage.__objects = {}
-            for key in objects:
-                name = key.split(".")[0]
-                FileStorage.__objects[key] = myDict[name](**objects[key])
+        try:
+            with open(FileStorage.__file_path, encoding="UTF8") as fd:
+                FileStorage.__objects = json.load(fd)
+            for key, val in FileStorage.__objects.items():
+                className = val["__class"]
+                className = models.classes[className]
+                FileStorage.__objects[key] = className(**val)
+        except FileNotFoundError:
+            pass
