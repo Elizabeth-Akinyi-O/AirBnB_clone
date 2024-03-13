0e1a1ff996140a944271e99f86f2b6f9139f4246
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2024-03-13 05:59:16 +0300

    Class BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -4,9 +4,9 @@ Contains BaseModel - Module, BaseModel Parent class
     serialization/deserialization information
 """
 
-import uuid
+import models
 from datetime import datetime
-from models import storage
+from uuid import uuid4
 
 
 class BaseModel():
@@ -16,44 +16,48 @@ class BaseModel():
     process of instances
     """
     def __init__(self, *args, **kwargs):
-        """Initialization of a BaseModel instance"""
-        if kwargs:
-            dateformat = "%Y-%m-%dT%H:%M:%S.%f"
-            k_dict = kwargs.copy()
-            del k_dict["__class__"]
-            for key in k_dict:
-                if (key == "created_at" or key == "updated_at"):
-                    k_dict[key] = datetime.strptime(k_dict[key], date_format)
-            self.__dict__ = k_dict
+        """Initialize a new BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        date_form = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, date_form)
+                else:
+                    self.__dict__[k] = v
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
-        """String representation of a BaseModel instance"""
-        return ("[{}] ({}) {}".format(self.__class__.__name__,
-                                      self.id, self.__dict__))
+        """Returns the str representation of a BaseModel instance"""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
 
-    def __repr__(self):
-        """Returns the string representation of class BaseModel"""
-        return ("[{}] ({}) {}".format(self.__class__.__name__,
-                                      self.id, self.__dict__))
+    # def __repr__(self):
+    # """Returns the string representation of class BaseModel"""
+    # return ("[{}] ({}) {}".format(self.__class__.__name__,
+    #                                  self.id, self.__dict__))
 
     def save(self):
         """Updates the public instance attribute updated_at
         with the current datetime"""
-        self.updated_at = datetime.now()
-        storage.save()
+        self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of
             __dict__ of the instance
         """
         newDct = self.__dict__.copy()
-        newDct['__class__'] = self.__class__.__name__
-        newDct['created_at'] = self.updated_at.isoformat()
-        newDct['updated_at'] = self.created_at.isoformat()
+        newDct["__class__"] = self.__class__.__name__
+        newDct["created_at"] = self.updated_at.isoformat()
+        newDct["updated_at"] = self.created_at.isoformat()
 
         return (newDct)
